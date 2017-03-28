e9c19efa0963fdddc0e93a68bebd4895a56814bd
Hong Chen <me@hongchen.cz>
2017-03-28 11:36:27 -0600

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
+rm -rf ~/.vim*
 ln -sf $PWD/vimrc ~/.vimrc
-
-ln -sf $PWD/gitconfig ~/.gitconfig
-
 mkdir -p ~/.vim/after; rm -rf ~/.vim/after/my_snippets; ln -sf $PWD/my_snippets ~/.vim/after/my_snippets
 
+rm -rf ~/.gitconfig
+ln -sf $PWD/gitconfig ~/.gitconfig
+
+rm -rf ~/.tmux.conf
 ln -sf $PWD/tmux.conf ~/.tmux.conf

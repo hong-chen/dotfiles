24bf2ef2271ae8d85a404d38d48dc49618d1d84b
Hong Chen <me@hongchen.cz>
2017-03-28 10:19:33 -0600

    add tmux.conf install in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/gitconfig ~/.gitconfig
 
 mkdir -p ~/.vim/after; rm -rf ~/.vim/after/my_snippets; ln -sf $PWD/my_snippets ~/.vim/after/my_snippets
+
+ln -sf $PWD/tmux.conf ~/.tmux.conf

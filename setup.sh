127aacaf4385acd4411abdbed8bf9242d6573e84
Hong Chen <me@hongchen.cz>
2017-04-11 23:01:12 -0600

    daily commit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-rm -rf ~/.vim*
-ln -sf $PWD/vimrc ~/.vimrc
+# rm -rf ~/.vim*
+# ln -sf $PWD/vimrc ~/.vimrc
 mkdir -p ~/.vim/after; rm -rf ~/.vim/after/my_snippets; ln -sf $PWD/data/my_snippets ~/.vim/after/my_snippets
 
 rm -rf ~/.gitconfig
